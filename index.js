38f23bdf6a652dd9e3aadced237142f7d9ed59d3
Nhan nguyen <nhan99443@donga.edu.vn>
2025-10-01 14:08:57 +0700

    update api message for CI/CD test

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 86;
 
-app.get('/api/hello', (req, res) => {
-  res.send('Hello from Node.js API with Docker & CI/CD!');
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "🚀 CI/CD working! Version 2 deployed." });
 });
 
 app.listen(port, () => {
